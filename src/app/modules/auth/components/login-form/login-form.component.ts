28c037a42381b8d2ac3ccebd8245683b874ea1fb
Omar Guanoluisa <omar.guanoluisa25@gmail.com>
2025-04-01 17:57:33 -0500

    login page was created

--- src/app/modules/auth/components/login-form/login-form.component.ts
+++ src/app/modules/auth/components/login-form/login-form.component.ts
@@ -3,11 +3,18 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss',
-  imports: [MatCardModule, MatFormFieldModule, MatButtonModule, MatInputModule],
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+  ],
 })
 export class LoginFormComponent {}
