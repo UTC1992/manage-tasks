54a47b3a21dabb44eaf456382ebc5b8533e721e3
Omar Guanoluisa <omar.guanoluisa25@gmail.com>
2025-04-01 18:35:22 -0500

    add props in item task

--- src/app/modules/tasks/components/task-item/task-item.component.ts
+++ src/app/modules/tasks/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -8,6 +8,18 @@ import { MatListModule } from '@angular/material/list';
   selector: 'app-task-item',
   imports: [MatCardModule, MatButtonModule, MatIconModule, MatListModule],
   templateUrl: './task-item.component.html',
-  styleUrl: './task-item.component.scss',
+  styleUrls: ['./task-item.component.scss'],
 })
-export class TaskItemComponent {}
+export class TaskItemComponent {
+  @Input() title: string;
+  @Input() description: string;
+  @Input() completed: boolean;
+  @Input() createdAt: Date;
+
+  constructor() {
+    this.title = '';
+    this.description = '';
+    this.completed = false;
+    this.createdAt = new Date();
+  }
+}
