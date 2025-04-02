fbc30a2530470761362ea65415c493723e933517
Omar Guanoluisa <omar.guanoluisa25@gmail.com>
2025-04-01 19:45:29 -0500

    add dialog

--- src/app/modules/tasks/components/task-form/task-form.component.ts
+++ src/app/modules/tasks/components/task-form/task-form.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import {
+  MAT_DIALOG_DATA,
+  MatDialogActions,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-task-form',
-  imports: [],
+  imports: [
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogActions,
+    MatButtonModule,
+  ],
   templateUrl: './task-form.component.html',
-  styleUrl: './task-form.component.scss'
+  styleUrl: './task-form.component.scss',
+  standalone: true,
 })
 export class TaskFormComponent {
-
+  dialogRef = inject(MatDialogRef<TaskFormComponent>);
+  data = inject<{
+    id: number;
+    name: string;
+  }>(MAT_DIALOG_DATA);
 }
--- src/app/modules/tasks/pages/task-home/task-home.component.ts
+++ src/app/modules/tasks/pages/task-home/task-home.component.ts
@@ -3,6 +3,12 @@ import { TaskListComponent } from '@app/modules/tasks/components/task-list/task-
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatButtonModule } from '@angular/material/button';
+import {
+  MatDialog,
+  MatDialogConfig,
+  MatDialogModule,
+} from '@angular/material/dialog';
+import { TaskFormComponent } from '@app/modules/tasks/components/task-form/task-form.component';
 
 @Component({
   selector: 'app-task-home',
@@ -11,8 +17,24 @@ import { MatButtonModule } from '@angular/material/button';
     MatIconModule,
     MatButtonModule,
     MatTooltipModule,
+    MatDialogModule,
   ],
   templateUrl: './task-home.component.html',
   styleUrl: './task-home.component.scss',
+  standalone: true,
 })
-export class TaskHomeComponent {}
+export class TaskHomeComponent {
+  constructor(private readonly dialog: MatDialog) {}
+
+  openDialog(): void {
+    const dialogConfig = new MatDialogConfig();
+
+    // Configure the dialog options
+    dialogConfig.disableClose = true; // Prevents closing the dialog by clicking outside
+    dialogConfig.autoFocus = false; // Disable autofocus to manually control focus
+    dialogConfig.width = '80%'; // Set the width of the dialog
+    dialogConfig.data = { id: 123, name: 'Angular' }; // Pass data to the dialog component
+    dialogConfig.autoFocus = 'input[name="testName"]'; //Pass autoFoucs field
+    this.dialog.open(TaskFormComponent, dialogConfig);
+  }
+}
